6922700c1b1047b239f63c977983104de69bee14
imparislaw <parisisthelaw@gmail.com>
2013-10-02 15:45:14 +0800

    added bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parisonrails::Application.config.secret_token = 'e5de5aa533ceeeefd01bcd9ead6c633b34a33da5547a75a9a450aecbe9b9387df53c591dc7c63f45d0ca763b3265fd7738d8467f02cbf7950d934d14e8ea8399'
